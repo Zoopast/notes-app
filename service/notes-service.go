baeaa362528fd13a6a06979388aa69e3de94f89e
Gabriel San Martin <gsandaur@hotmail.com>
2023-10-06 14:40:11 -0300

    feat(note-service): add notes service for repository communication

---
+++ service/notes-service.go
@@ -0,0 +1,38 @@
+package service
+
+import (
+	"github.com/Zoopast/notes-app/model"
+	"github.com/Zoopast/notes-app/repository"
+)
+
+type NoteService interface {
+	Save(model.Note) error
+	Update(model.Note) error
+	Delete(model.Note) error
+	FindAll() []model.Note
+}
+
+type noteService struct {
+	repository repository.NoteRepository
+}
+
+func New(noteRepository repository.NoteRepository) NoteService {
+	return &noteService{
+		repository: noteRepository,
+	}
+}
+func (service *noteService) Save(note model.Note) error {
+	service.repository.Save(note)
+	return nil
+}
+func (service *noteService) Update(note model.Note) error {
+	service.repository.Update(note)
+	return nil
+}
+func (service *noteService) Delete(note model.Note) error {
+	service.repository.Delete(note)
+	return nil
+}
+func (service *noteService) FindAll() []model.Note {
+	return service.repository.FindAll()
+}
